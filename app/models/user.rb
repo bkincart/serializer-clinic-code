43a0c886208616fbf25ec0a33b6293ee60f82ccc
Brianna <briannakincart@gmail.com>
2018-05-08 08:52:09 -0400

    Add follows to book app

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ has_many :follower_follows, class_name: "Follow", foreign_key: :follower_id
+ has_many :followeds, through: :follower_follows, source: :followed
+
+ has_many :followed_follows, class_name: "Follow", foreign_key: :followed_id
+ has_many :followers, through: :followed_follows, source: :follower
 end
