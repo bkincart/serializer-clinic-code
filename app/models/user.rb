9c73d35b12c07772402b4269a9bf2c547f5d2bdb
Brianna Kincart <briannakincart@gmail.com>
2019-02-04 15:18:28 -0500

    Add follows

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         def full_name
-           "#{first_name} #{last_name}"
-         end
+  has_many :follows_as_followed_person, class_name: "Follow", foreign_key: "followed_id"
+  has_many :followers, through: :follows_as_followed_person, source: :follower
+
+  has_many :follows_as_follower, class_name: "Follow", foreign_key: "follower_id"
+  has_many :followed_people, through: :follows_as_follower, source: :followed
+
+ def full_name
+   "#{first_name} #{last_name}"
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024180520) do
+ActiveRecord::Schema.define(version: 20190204165442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20171024180520) do
     t.string "publication"
     t.index ["author_id"], name: "index_books_on_author_id"
     t.index ["creator_id"], name: "index_books_on_creator_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.bigint "follower_id"
+    t.bigint "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
   create_table "reviews", force: :cascade do |t|
